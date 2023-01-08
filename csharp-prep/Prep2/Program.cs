44abde807702272dc2b62538d81ad98fd9c31883
elilaiono <elilaiono@gmail.com>
2023-01-07 18:46:00 -0700

    prep assignments 1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+             letter = "A";
+        }
+        else if (grade >= 80)
+        {
+             letter = "B";
+        }
+        else if (grade >= 70)
+        {
+             letter = "C";
+        }
+        else if (grade >= 60)
+        {
+             letter = "D";
+        }
+        else 
+        {
+             letter = "F";
+        }
+
+        Console.WriteLine($"You recieved a/an {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed the class!! Well done!");
+        }
+        else 
+        {
+            Console.WriteLine("You let down your ancestors who came to America seeking a better life for their posterity. YOU FAILED!!!");
+        }
     }
 }
