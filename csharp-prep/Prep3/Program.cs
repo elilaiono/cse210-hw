1e0a2e72ca3ac5a6b5b3c5ee421bffd05ec44e67
elilaiono <elilaiono@gmail.com>
2023-01-09 21:08:33 -0700

    prep assignments 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        // Console.WriteLine($"Your number is {number}");
+        bool guessed = false;
+        int turns = 0;
+        while(guessed == false)
+        {
+        Console.Write("Please enter your guess: ");
+        int guess = int.Parse(Console.ReadLine());
+        turns += 1;
+        if (guess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else 
+        {
+            Console.WriteLine("Correct");
+            guessed = true;
+        }
+        Console.WriteLine($"They guessed {guess}");
+        }
+        Console.WriteLine($"It took you {turns} turns to guess the number");
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of number, type 0 when finsihed");
+        List <int> numbers;
+        numbers = new List<int>();
+
+        int userInput = -1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Please enter a number: ");
+            string response = Console.ReadLine();
+            userInput = int.Parse(response);
+            
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+            else 
+            {
+                Console.WriteLine($"The sum is {numbers.Sum()}");
+                Console.WriteLine($"The average is {numbers.Average()}");
+                Console.WriteLine($"The highest number is {numbers.Max()}");
+            }
+            
+        }
+            
     }
 }
